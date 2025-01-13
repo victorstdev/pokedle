c5d264421f64955d97b480f1f5d25f9fdb3b8d63
victorstdev <victorstdev@gmail.com>
2025-01-13 08:02:48 -0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const search = document.getElementById('inpSearch');
-const url = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
-const randomPokemon = Math.floor(Math.random() * 151) + 1;
+const randomPokemonID = Math.floor(Math.random() * 151) + 1;
 
 search.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
-    const pokemon = search.value.toLowerCase();
-    console.log(pokemon, randomPokemon)
+    const pokemonUserChoice = search.value.toLowerCase();
   }
 });
